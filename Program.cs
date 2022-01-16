6863bf28149994b775eedb6e07a31833d6039ddb
kienvu01 <78364832+kienvu01@users.noreply.github.com>
2022-01-17 00:03:55 +0700

    ok

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 enum Genders { Male, Female }
 class Member
@@ -54,91 +55,199 @@ namespace Assignment_1
             members.Add(new Member() { FirstName = "Eleanor  ", LastName = "Shepard", Gender = Genders.Male, PhoneNumber = "(630) 365-2842", Birthday = DateTime.Parse("09/08/2001"), Age = 21, BirthPlace = "Hanoi", IsGraduated = true });
             members.Add(new Member() { FirstName = "Melissa  ", LastName = "Singh", Gender = Genders.Female, PhoneNumber = "(706) 517-0664", Birthday = DateTime.Parse("06/29/1998"), Age = 24, BirthPlace = "Hanoi", IsGraduated = true });
             members.Add(new Member() { FirstName = "Alexa  ", LastName = "Simson", Gender = Genders.Female, PhoneNumber = "(126) 321-321", Birthday = DateTime.Parse("06/29/2000"), Age = 22, BirthPlace = "Hanoi", IsGraduated = true });
-
-            // Lay full name
-            Console.WriteLine();
-            Console.WriteLine("All members' full name:");
-            foreach (Member i in members)
+            
+            for(;;)
             {
+            Console.WriteLine("Press any key to start:");
+            Console.ReadLine();
+            Console.WriteLine("Choose the task you want to do:");
+            Console.WriteLine("1. Add a member to the class.");
+            Console.WriteLine("2. Get full name of all members.");
+            Console.WriteLine("3. Get all male member.");
+            Console.WriteLine("4. Find the oldest member.");
+            Console.WriteLine("5. Sort members at year point 2000.");
+            Console.WriteLine("6. First person in class from Hanoi.");
 
-                Console.WriteLine(i.GetName());
-            }
-            //Cac member la nam
-            Console.WriteLine("############################");
-            Console.WriteLine("List Male members:");
-            foreach (Member i in members)
+            int task = Int32.Parse(Console.ReadLine());
+            switch(task)
             {
-                if (i.Gender == Genders.Male)
+                case 1:
+                Console.WriteLine("Enter member's first name:");
+                string firstname = Console.ReadLine();
+                Console.WriteLine("Enter member's last name:");
+                string lastname = Console.ReadLine();
+                Console.WriteLine("Enter member's gender: 1:Male; 2:Female");
+                int gender = Int32.Parse(Console.ReadLine());
+                Genders mGender = Genders.Male;
+                int genderloop = 0;
+                while(genderloop != 1){
+                switch(gender)
                 {
+                    case 1: 
+                        mGender = Genders.Male;
+                        genderloop = 1;
+                        break;
+                    case 2: 
+                        mGender = Genders.Female;
+                        genderloop = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Enter member's gender: 1:Male; 2:Female");
+                        gender = Int32.Parse(Console.ReadLine());
+                        break;
+                }
+                }
+                Console.WriteLine("Enter nember's phone number");
+                string phone = Console.ReadLine();
+                
+                Console.WriteLine("Enter member's birthday(mm/dd//yyyy)");
+                string rawBirthday = Console.ReadLine();
+                DateTime birthday;
+                bool checkFormat = DateTime.TryParse(rawBirthday, out birthday);
+                while(checkFormat == false)
+                {
+                    Console.WriteLine("Wrong format, plase enter birthday correctly(mm/dd/yyyy):");
+                    rawBirthday = Console.ReadLine();
+                    checkFormat = DateTime.TryParse(rawBirthday, out birthday);
+                }
+                birthday = DateTime.Parse(rawBirthday);
+                int age = 2022- birthday.Year;
+                Console.WriteLine("Enter member birthplace:");
+                string birthplace = Console.ReadLine();
+                Console.WriteLine("Is member graduated?");
+                Console.WriteLine("y(Yes) n(No)");
+                string checkGraduate = Console.ReadLine();
+                bool graduate = true;
+                int graduateLoop = 0;
+                while(graduateLoop != 1)
+                {   
+                   
+                    switch(checkGraduate)
+                    {
+                        case "y":
+                        graduate = true;
+                        graduateLoop = 1;
+                        break;
+                        case "n":
+                        graduate = false;
+                        graduateLoop = 1;
+                        break;
+                        default:
+                        Console.WriteLine("Is member graduated?");
+                        Console.WriteLine("y(Yes) n(No)");
+                        checkGraduate = Console.ReadLine();
+                        break;
+                    }
+                }
+                members.Add(new Member() { FirstName = firstname, LastName = lastname, Gender = mGender, PhoneNumber = phone, Birthday = birthday, Age = age, BirthPlace = birthplace, IsGraduated = graduate });
+                Console.WriteLine("Added member:");
+                Console.WriteLine(members[members.Count-1].getInfor());
+                break;
+                case 2:
+                // Lay full name
+                Console.WriteLine();
+                Console.WriteLine("All members' full name:");
+                foreach (Member i in members)
+                {
+
                     Console.WriteLine(i.GetName());
                 }
-            }
-            //Member gia nhat
-            Console.WriteLine("############################");
-            Console.WriteLine("Oldest person is:");
-            Member oldest = members[0];
-            for (int i = 0; i < members.Count; i++)
-            {
-                if (members[i].Age > oldest.Age)
+                break;
+                case 3:
+                //Cac member la nam
+                Console.WriteLine("############################");
+                Console.WriteLine("List Male members:");
+                foreach (Member i in members)
                 {
-                    oldest = members[i];
+                    if (i.Gender == Genders.Male)
+                    {
+                        Console.WriteLine(i.GetName());
+                    }
+                }
+                break;
+                case 4:
+                 //Member gia nhat
+                Console.WriteLine("############################");
+                Console.WriteLine("Oldest person is:");
+                Member oldest = members[0];
+                for (int i = 0; i < members.Count; i++)
+                {
+                    if (members[i].Age > oldest.Age)
+                    {
+                        oldest = members[i];
+                    }
+
+                }
+                Console.WriteLine(oldest.GetName());
+                break;
+
+                case 5:
+                
+                //Phan loai member theo nam sinh
+                Console.WriteLine("############################");
+                List<Member> membersIn2000 = new List<Member>();
+                List<Member> membersBefore2000 = new List<Member>();
+                List<Member> membersAfter2000 = new List<Member>();
+                foreach (Member i in members)
+                {
+                    switch (i.Birthday.Year)
+                    {
+                        case int n when n > 2000:
+                            membersAfter2000.Add(i);
+                            break;
+                        case int n when n == 2000:
+                            membersIn2000.Add(i);
+                            break;
+                        case int n when n < 2000:
+                            membersBefore2000.Add(i);
+                            break;
+                    }
+
+                }
+                Console.WriteLine("Member before 2000:");
+                foreach (Member i in membersBefore2000)
+                {
+
+                    Console.WriteLine(i.getInfor());
+                }
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Member In 2000:");
+                foreach (Member i in membersIn2000)
+                {
+
+                    Console.WriteLine(i.getInfor());
+                }
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Member In 2000:");
+                foreach (Member i in membersAfter2000)
+                {
+
+                    Console.WriteLine(i.getInfor());
+                }
+                break;
+                case 6:
+                // Nguoi dau tien trong list sinh o Hanoi
+                
+                Console.WriteLine("First person born in Hanoi is:");
+                int hn = 0;
+                while (members[hn].BirthPlace != "Hanoi")
+                    {
+                        hn++;
+                    }
+                Console.WriteLine(members[hn].getInfor());
+                break;
+                default:
+                break;
+
                 }
 
-            }
-            Console.WriteLine(oldest.GetName());
-
-            //Phan loai member theo nam sinh
-            Console.WriteLine("############################");
-            List<Member> membersIn2000 = new List<Member>();
-            List<Member> membersBefore2000 = new List<Member>();
-            List<Member> membersAfter2000 = new List<Member>();
-            foreach (Member i in members)
-            {
-                switch (i.Birthday.Year)
-                {
-                    case int n when n > 2000:
-                        membersAfter2000.Add(i);
-                        break;
-                    case int n when n == 2000:
-                        membersIn2000.Add(i);
-                        break;
-                    case int n when n < 2000:
-                        membersBefore2000.Add(i);
-                        break;
-                }
-
-            }
-            Console.WriteLine("Member before 2000:");
-            foreach (Member i in membersBefore2000)
-            {
-
-                Console.WriteLine(i.getInfor());
-            }
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("Member In 2000:");
-            foreach (Member i in membersIn2000)
-            {
-
-                Console.WriteLine(i.getInfor());
-            }
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("Member In 2000:");
-            foreach (Member i in membersAfter2000)
-            {
-
-                Console.WriteLine(i.getInfor());
-            }
 
 
-            // Nguoi dau tien trong list sinh o Hanoi
-            Console.WriteLine("############################");
-            Console.WriteLine("First person born in Hanoi");
-            int hn = 0;
-            while (members[hn].BirthPlace != "Hanoi")
-            {
-                hn++;
-            }
-            Console.WriteLine(members[hn].getInfor());
+            
+           
+
+         
+        }
         }
 
     }
